9da5e7cf8567da24816e28b5b54549fa72366846
Lucas Bonomo <bonomo.lucas@gmail.com>
2024-06-17 23:52:15 -0300

    Day 07

--- Week-01/Day-07_Merge-Sorted-Array/src/lib.rs
+++ Week-01/Day-07_Merge-Sorted-Array/src/lib.rs
@@ -1,13 +1,14 @@
-pub fn merge(nums1:Vec<u8>, nums2:Vec<u8>) -> Vec<u8> {
-    let mut result:Vec<u8>  = vec![];
-    
-    for num in nums1.iter() {
-        println!("{}", num );
-        result.push(num);
-        // if num > 0 {
-        // }
-    };
+pub fn method_01(nums1:Vec<u8>, nums2:Vec<u8>) -> Vec<u8> {
+    let mut result:Vec<u8>  = nums1[0..nums2.len()].to_vec(); 
 
+    result.extend_from_slice(&nums2);
+    result.sort();
+    return result
+}
+
+pub fn method_02(m:Vec<u8>,n:Vec<u8>) -> Vec<u8> {
+    let mut result:Vec<u8> = [&m[..n.len()], &n].concat();
+    result.sort();
     return result
 }
 
@@ -21,7 +22,17 @@ mod tests {
         let nums2 = vec![2,5,6];
         let output = vec![1,2,2,3,5,6];
 
-        let result = merge(nums1, nums2);
+        let result = method_01(nums1, nums2);
+        assert_eq!(result, output);
+    }
+
+    #[test]
+    fn it_works2() {
+        let nums1 = vec![1,2,3,0,0,0];
+        let nums2 = vec![2,5,6];
+        let output = vec![1,2,2,3,5,6];
+
+        let result = method_02(nums1, nums2);
         assert_eq!(result, output);
     }
 }

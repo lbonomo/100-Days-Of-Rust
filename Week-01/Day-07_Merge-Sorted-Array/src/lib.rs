840a725ef53981f12cb93f9c27a75bd8a8d8771d
Lucas Bonomo <bonomo.lucas@gmail.com>
2024-06-22 18:52:27 -0300

    Fixing. Thanks @sebastianpennino

--- Week-01/Day-07_Merge-Sorted-Array/src/lib.rs
+++ Week-01/Day-07_Merge-Sorted-Array/src/lib.rs
@@ -1,13 +1,15 @@
-pub fn method_01(nums1:Vec<u8>, nums2:Vec<u8>) -> Vec<u8> {
-    let mut result:Vec<u8>  = nums1[0..nums2.len()].to_vec(); 
+pub fn method_01(nums1:Vec<i8>, nums2:Vec<i8>) -> Vec<i8> {
+    let size = nums1.len() - nums2.len(); 
+    let mut result:Vec<i8>  = nums1[0..size].to_vec(); 
 
     result.extend_from_slice(&nums2);
     result.sort();
     return result
 }
 
-pub fn method_02(m:Vec<u8>,n:Vec<u8>) -> Vec<u8> {
-    let mut result:Vec<u8> = [&m[..n.len()], &n].concat();
+pub fn method_02(m:Vec<i8>,n:Vec<i8>) -> Vec<i8> {
+    let size = m.len() - n.len();
+    let mut result:Vec<i8> = [&m[..size], &n].concat();
     result.sort();
     return result
 }
@@ -17,7 +19,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn it_works1() {
         let nums1 = vec![1,2,3,0,0,0];
         let nums2 = vec![2,5,6];
         let output = vec![1,2,2,3,5,6];
@@ -31,6 +33,26 @@ mod tests {
         let nums1 = vec![1,2,3,0,0,0];
         let nums2 = vec![2,5,6];
         let output = vec![1,2,2,3,5,6];
+
+        let result = method_02(nums1, nums2);
+        assert_eq!(result, output);
+    }
+
+    #[test]
+    fn it_works3() {
+        let nums1 = vec![1,2,3,0,0,0,0];
+        let nums2 = vec![-2,5,6,7];
+        let output = vec![-2,1,2,3,5,6,7];
+
+        let result = method_01(nums1, nums2);
+        assert_eq!(result, output);
+    }
+
+    #[test]
+    fn it_works4() {
+        let nums1 = vec![1,2,3,0,0,0,0];
+        let nums2 = vec![-2,5,6,7];
+        let output = vec![-2,1,2,3,5,6,7];
 
         let result = method_02(nums1, nums2);
         assert_eq!(result, output);

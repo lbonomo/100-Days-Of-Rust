98728872204daa5a2ea0269ca8b3ace6f6fa6199
Lucas Bonomo <bonomo.lucas@gmail.com>
2024-06-24 15:31:03 -0300

    Lazy static

--- Week-02/Day-08_Letter-Combinations-Of-A-Phone-Number/src/lib.rs
+++ Week-02/Day-08_Letter-Combinations-Of-A-Phone-Number/src/lib.rs
@@ -1,33 +1,42 @@
-use std::{char, collections::HashMap};
+use std::collections::HashMap;
 
+use lazy_static::lazy_static;
+lazy_static! {
+    static ref DIGITS: HashMap<u8, Vec<&'static str>> = HashMap::from([
+        (2, vec!["a", "b", "c"]),
+        (3, vec!["d", "e", "f"]),
+        (4, vec!["g", "h", "i"]),
+        (5, vec!["j", "k", "l"]),
+        (6, vec!["m", "n", "o"]),
+        (7, vec!["p", "q", "r", "s"]),
+        (8, vec!["t", "u", "v"]),
+        (9, vec!["w", "x", "y", "z"])
+    ]);
+}
 
-enum Option<T>{ Some(T), None }
-
-const DIGITS:HashMap<u8, Vec<char>> = HashMap::from([
-    (2, vec!['a', 'b', 'c']),
-    (3, vec!['d', 'e', 'f']),
-    (4, vec!['g', 'h', 'i']),
-    (5, vec!['j', 'k', 'l']),
-    (6, vec!['m', 'n', 'o']),
-    (7, vec!['p', 'q', 'r', 's']),
-    (8, vec!['t', 'u', 'v']),
-    (9, vec!['w', 'x', 'y', 'z']), 
-]);
-
-
-pub fn add(digits:&str) -> Vec<&str> {
+pub fn add(digits:u8) -> Vec<&'static str> {
     // - `0 <= digits.length <= 4`
     // - `digits[i] is a digit in the range ['2', '9'].`
 
-    if digits.len() == 0 { return vec![]; }
+    // if digits.len() == 0 { return vec![]; }
+ 
 
-    // if digits.len() == 1 { return DIGITS[digits] }
+    if digits == 2 {
+        let words = DIGITS[&digits]; 
+        return  words
+    } else {
+        let words = DIGITS[&digits];
+        return words
+    }
 
-    // for n in digits {
-    //     print!("{}", n);
+    // match dig.get(&4) {
+    //     Some(d) => return d,
+    //     None => println!("is unreviewed.")
     // }
-}
 
+    // return vec!["a", "b", "c"]
+    
+}
 /**
  * 2: a b c
  * 3: d e f
@@ -48,7 +57,7 @@ mod tests {
     fn test_01() {
         // Input: digits = "23"
         // Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
-        assert_eq!(add("23"), vec!["ad","ae","af","bd","be","bf","cd","ce","cf"]);
+        assert_eq!(add(23), vec!["ad","ae","af","bd","be","bf","cd","ce","cf"]);
     }
 
     // #[test]
@@ -62,6 +71,6 @@ mod tests {
     fn test_03() {
         // Input: digits = "2"
         // Output: ["a","b","c"]
-        assert_eq!(add("2"), vec!["a","b","c"]);
+        assert_eq!(add(2), vec!["a","b","c"]);
     }    
 }

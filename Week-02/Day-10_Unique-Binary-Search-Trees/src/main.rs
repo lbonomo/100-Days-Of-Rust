8dc9de0cb5cbedaeabe1729100ec76cb8c4fde67
Lucas Bonomo <bonomo.lucas@gmail.com>
2024-06-27 08:17:51 -0300

    Working with structs

--- Week-02/Day-10_Unique-Binary-Search-Trees/src/main.rs
+++ Week-02/Day-10_Unique-Binary-Search-Trees/src/main.rs
@@ -1,37 +1,40 @@
 use serde::Deserialize;
 
-#[derive(Deserialize, Debug)]
-pub struct Trees {
-    pub id: u8,
-    pub tree: Tree,
-}
+// #[derive(Deserialize, Debug)]
+// pub struct Trees {
+//     pub id: u8,
+//     pub tree: Tree,
+// }
 
 #[derive(Deserialize, Debug)]
 pub struct Tree {
-    pub root: i8,
-    pub levels: Level,
+    pub root: u8,
+    pub levels: Option<Vec<Level>>,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct Level {
     pub level: u8,
-    pub left: u8,
-    pub right: u8,
+    pub left: Option<u8>,
+    pub right: Option<u8>,
 }
 
 
 fn main() {
-    // let trees:Trees= todo!();
-
+    
     const INPUT:u8= 3;
+    let mut trees:Vec<Tree> = Vec::new();
     
-    // let num:[u8; INPUT as usize]
-    let mut num:Vec<u8> = Vec::new();
-
     for n in 1..INPUT+1 {
-        num.push(n);    
-    } 
+        let data:Tree = Tree {
+            root:n,
+            levels: None,
+        };
+        trees.push(data);
+    }
     
     
-    println!("{:?}", num);
+    
+    
+    println!("{:?}", trees);
 }

2507bdf729a3b7cc35f30f32639da3dd866c5b2c
Lucas Bonomo <bonomo.lucas@gmail.com>
2024-07-04 22:05:01 -0300

    Working with struct

--- Week-02/Day-10_Unique-Binary-Search-Trees/src/main.rs
+++ Week-02/Day-10_Unique-Binary-Search-Trees/src/main.rs
@@ -1,85 +1,27 @@
-use std::ptr::null;
-
-use serde::Deserialize;
-
-#[derive(Deserialize, Debug)]
-pub struct Tree {
-    pub root: Option<u8>,
-    pub left: Option<Box<Tree>>,
-    pub right: Option<Box<Tree>>,
+#[derive(Debug)]
+pub struct Branch {
+    pub father: Option<u8>,
+    pub left: Option<Box<Branch>>,
+    pub right: Option<Box<Branch>>,
 }
 
-// #[derive(Deserialize, Debug)]
-// pub struct Branch {
-//     pub level: u8,
-//     pub left: Option<Box<Branch>>,
-//     pub right: Option<Box<Branch>>,
-// }
-
-
-// fn make_level(father: u8, level:u8, nums:Vec<u8>) {
-//     println!("{:?}", nums);
-// }
-
-
 fn main() {
-   
-    // const INPUT:u8 = 3;
-    // let mut trees:HashMap<u8, Tree> = HashMap::new();
-
-    let tree1:Tree = Tree { 
-            root: Some(8), 
-            left: Some(Box::new(Tree {
-                root: Some(1), 
-                left: Some(4), 
-                right: Some(5)
+    let tree1:Branch = Branch { 
+            father: Some(8), 
+            left: Some(Box::new(Branch {
+                father: Some(1), 
+                left: None, 
+                right: Some(Box::new(Branch{
+                    father:Some(5), 
+                    left: None, 
+                    right: None
+                }))
             })), 
-            right: Some(Tree {
-                level: 1, 
-                left: Box::new(10), 
-                right: Some(None) 
-            })
+            right: Some(Box::new(Branch {
+                father: Some(1), 
+                left: None, 
+                right: None 
+            }))
         };
     dbg!(tree1);
-
-
-    // print!("{:?}", tree1);
-
-    // Make a array of `u8` from 1 to INPUT.
-    // Ex: INPUT = 3 -> [1, 2, 3]
-    // let numbers:[u8; INPUT as usize] =  (1..=INPUT).collect::<Vec<_>>().try_into().expect("wrong size iterator");
-
-    // println!("{:?}", numbers);
-
-    // for n in numbers {
-    //     let id = n;
-    //     let level_number = 0;
-        
-    //     // println!("{}", n);
-
-
-
-    //     let mut nums = numbers.to_vec();
-    //     // nums.remove(id as usize);
-    //     // nums.extract_if(|x| x == &n );
-        
-    //     // println!("{:?}", nums);
-
-    //     // make_level(n, level_number, nums);
-    //     // if level_number == 0 {
-    //     //     // Level 0 / Root.
-    //     //     let level = make_level(n, level_number, nums);
-    //     //     let tree:Tree = Tree {
-    //     //         root:n,
-    //     //         levels: None,
-    //     //     };
-    //     // }
-        
-
-        
-    //     // trees.insert(id, tree);
-    // }
-
-    
-    // println!("{:?}", trees);
 }
